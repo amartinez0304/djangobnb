acc8af1a9e9d5d29b45016b187df8f645379bb94
amartinez0304 <a.martinez0304@gmail.com>
2024-05-12 00:37:23 +0200

    created the navigation bar for web version

---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Image from "next/image";
+import SearchFilters from "./searchFilters";
+const Navbar = () =>{
+    return(
+        <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
+            <div className="max-w-[1500px] mx-auto px-6">
+                <div className="flex justify-between items-center">
+                    <Link href ="/">
+                        <Image 
+                            src="/logo.png"
+                            alt="DjangoBnb"
+                            width={180}
+                            height={380}
+                            />
+                    </Link>
+                    <div className="flex space-x-6">
+                        <SearchFilters />
+                    </div>
+                    <div className="flex items-center space-x-6">
+                        Add property - user nav
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
